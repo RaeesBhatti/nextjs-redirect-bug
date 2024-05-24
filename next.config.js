6aba4fe6f9d44a2c1cc62915ff79dc1f0fd78431
Raees Iqbal <10067728+RaeesBhatti@users.noreply.github.com>
2024-05-24 19:07:36 -0400

    Add bug causing code

--- next.config.js
+++ next.config.js
@@ -1,8 +1,17 @@
 // This file was automatically added by edgio init.
 // You should commit this file to source control.
-const { withEdgio } = require('@edgio/next/config')
+const { withEdgio } = require("@edgio/next/config");
 
 module.exports = (phase, config) =>
   withEdgio({
     // Write your Next config here
-  })
+    async redirects() {
+      return [
+        {
+          source: "/promotion-new",
+          destination: "https://uniform-sales-demo-default.glb.edgio.link/JavaDrip-Specialty-Launch",
+          permanent: true,
+        },
+      ];
+    },
+  });
